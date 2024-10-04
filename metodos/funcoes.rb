fbe36a60927cb0b19bf074a80706ced4476269a8
Tibério dos Santos Ferreira <tiberio.ferreiracs@gmail.com>
2024-10-04 18:41:20 -0300

    função que calcula o maior valor entre dois números

--- metodos/funcoes.rb
+++ metodos/funcoes.rb
@@ -22,7 +22,7 @@ def average(n)
         puts "Sua lista contém um número: #{n}"
     else
         # soma dos números
-        # 0.0 inicia a soma como float para evitar problemas com divisão
+        # a variavel sum = 0.0 inicia a soma como float para evitar problemas com divisão
         sum = 0.0     
         # o "for" usa a variável "sum" para acumular a soma de cada elemento do array
         for num in n do
@@ -34,9 +34,37 @@ def average(n)
     end
 end
 
-average(num)
-average(numbers)
-average(numbersLot)
-average(empty)
-average(mix)
-average(strings)
+# average(num)
+# average(numbers)
+# average(numbersLot)
+# average(empty)
+# average(mix)
+# average(strings)
+
+# Usar funções em combinação com condicionais para resolver problemas mais complexos.
+# Exemplo: função que calcula o maior valor entre dois números.
+
+def compare(n, n1)
+    # verifica se os dados são números
+    if n.is_a?(String) or n1.is_a?(String)
+        puts "Verifique os dados, eles devem ser números!"
+    else
+        # retorna o 1 número, caso ele for maior que o 2
+        if n > n1 
+            puts "O valor é maior #{n}"
+        # retorna o 2 número, caso ele for maior que o 1    
+        elsif n1 > n
+            puts "O valor é maior #{n1}"
+        # retorna qualquer um dos números, caso eles forem iguais    
+        elsif n == n1
+            puts "Os dois números são iguais #{n or n1}"
+        end
+    end
+end
+
+compare("tiberio", 33)
+compare(33, "tiberio")
+compare(10, 20)
+compare(30, 20)
+compare(20, 20)
+compare(5.5, 8.5)
