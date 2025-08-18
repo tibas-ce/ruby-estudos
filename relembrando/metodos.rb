fd290776e2f8ada9636a1a69c3313151d85c86b8
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-08-18 14:25:54 -0300

    relembrando metodos

---
+++ relembrando/metodos.rb
@@ -0,0 +1,11 @@
+# Crie um método que receba o nome de uma pessoa e retorne uma saudação personalizada.
+
+# Variável 
+nome = gets.chomp
+
+def saudar_pessoa(nome)
+    puts "Olá #{nome}! bem vindo(a)!"
+    return 
+end 
+
+saudar_pessoa(nome)
