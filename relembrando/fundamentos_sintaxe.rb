67475742b5b1de16728e82b03d410966f00311fc
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-08-18 09:08:57 -0300

    relmbrando operadores e estruturas de comtrole

--- relembrando/fundamentos_sintaxe.rb
+++ relembrando/fundamentos_sintaxe.rb
@@ -1,9 +1,11 @@
 #Crie variáveis para representar seu nome, idade, cidade e hobby,
 # e imprima uma frase com interpolação.
 
+# Variáveis
 nome = "Tibério dos Santos Ferreira"
 idade = 33
 cidade = "Campos Sales - CE"
 hobby = "Motociclismo"
 
+# Saída 
 puts "Eu #{nome}, tenho #{idade} anos. Sou de #{cidade}, e adoro #{hobby}."
