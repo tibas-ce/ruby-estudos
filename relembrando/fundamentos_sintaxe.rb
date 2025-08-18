05558acfc45cabe6a0767264868138aa5b2cf288
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-08-18 08:49:09 -0300

    relmbrando fundamentos e sintaxe

---
+++ relembrando/fundamentos_sintaxe.rb
@@ -0,0 +1,9 @@
+#Crie variáveis para representar seu nome, idade, cidade e hobby,
+# e imprima uma frase com interpolação.
+
+nome = "Tibério dos Santos Ferreira"
+idade = 33
+cidade = "Campos Sales - CE"
+hobby = "Motociclismo"
+
+puts "Eu #{nome}, tenho #{idade} anos. Sou de #{cidade}, e adoro #{hobby}."
